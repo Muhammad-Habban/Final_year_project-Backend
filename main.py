57a66ca1e1af7c41b82705d03f9c51991723f28d
Muhammad-Habban <mhabban2004@gmail.com>
2025-03-15 14:27:36 +0500

    Add CORS

--- main.py
+++ main.py
@@ -3,9 +3,19 @@ from controllers.user_controller import router as user_router
 from controllers.message_controller import router as message_router
 from database import get_database, client
 from controllers.chat_controller import router as chat_router
-
+from fastapi.middleware.cors import CORSMiddleware
+origins = [
+    "http://localhost:3000",
+]
 app = FastAPI()
-
+#CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 # Include routers
 app.include_router(user_router)
 app.include_router(message_router)
